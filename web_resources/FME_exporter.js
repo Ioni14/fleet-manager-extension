ef133361107a4bee49067d7cbc364081f5fe1e53
Thomas Talbot <thomas.talbot@fauya.fr>
2019-05-04 12:30:47 +0200

    add UX errors

--- web_resources/FME_exporter.js
+++ web_resources/FME_exporter.js
@@ -1,17 +1,26 @@
 (function () {
-    const fleetManagerBaseUrl = 'https://fleet-manager.traefik.test'; // fleet.fallkrom.space
+    'use strict';
+    const fleetManagerBaseUrl = 'https://fleet.fallkrom.space';
 
     function createExporterBlock() {
         const exporterBlockHtml = `
-        <div id="FME-exporter-block" style="margin-top:20px;">
-            <a class="shadow-button trans-02s trans-color" id="FME-exporter-submit">
-                <span class="label js-label trans-02s">Export to Fleet Manager</span>
-                <span class="icon trans-02s"></span>
-                <span class="left-section"></span>
-                <span class="right-section"></span>
-            </a>
-        </div>
-    `;
+            <div id="FME-exporter-block" style="margin-top:20px;">
+                <a class="shadow-button trans-02s trans-color" id="FME-exporter-submit">
+                    <span class="label js-label trans-02s">Export to Fleet Manager</span>
+                    <span class="icon trans-02s"></span>
+                    <span class="left-section"></span>
+                    <span class="right-section"></span>
+                </a>
+                <p id="FME-exporter-msg" style="
+                    color: #6c84a2;
+                    font-size: 12px;
+                    text-transform: uppercase;
+                    font-family: 'Electrolize', sans-serif;
+                    display: inline-block;
+                    padding-left: 4px;
+                "></p>
+            </div>
+        `;
         $('.sidenav').append(exporterBlockHtml);
     }
 
@@ -70,7 +79,7 @@
             return null;
         }
         if (resp.redirected && resp.url === fleetManagerBaseUrl + '/login/') {
-            $('#FME-exporter-block').append(`<p id="FME-exporter-msg">We can't upload your fleet. Please login first at <a href="${fleetManagerBaseUrl}" target="_blank">${fleetManagerBaseUrl}</a>.</p>`);
+            $('#FME-exporter-msg').html(`We can't upload your fleet. Please login first at <a href="${fleetManagerBaseUrl}" target="_blank">${fleetManagerBaseUrl}</a>.`);
             return null;
         }
         const json = await resp.json();
@@ -79,7 +88,7 @@
     }
 
     $('#FME-exporter-submit').on('click', async (ev) => {
-        $('#FME-exporter-msg, #FME-exporter-success').remove();
+        $('#FME-exporter-msg').html('');
 
         let apiToken = window.localStorage.getItem('apiToken');
         if (!apiToken) {
@@ -90,6 +99,7 @@
             return;
         }
 
+        $('#FME-exporter-msg').html(`Uploading...`);
         const resp = await fetch(fleetManagerBaseUrl+'/export', {
             method: 'POST',
             headers: {
@@ -99,16 +109,39 @@
             body: JSON.stringify(pledges)
         });
         if (resp.ok) {
-            $('#FME-exporter-block').append(`<p id="FME-exporter-success">Success!</p>`);
+            $('#FME-exporter-msg').html(`<span style="color: #0f0;">Success!</span> <a target="_blank" href="${fleetManagerBaseUrl}/#/my-fleet">Go to your fleet</a>.`);
             return;
         }
         if (resp.status === 400) {
             const json = await resp.json();
             console.error(`An error has occurred. Error: ${json.error}.`);
-            $('#FME-exporter-block').append(`<p id="FME-exporter-msg">An error has occurred, please retry. Error: ${json.error}.</p>`);
+
+            let errorHtml = '';
+            switch (json.error) {
+                case 'no_citizen_created':
+                    errorHtml = `Your RSI account must be linked first. Go to the <a target="_blank" href="${fleetManagerBaseUrl}/#/profile">profile page</a>.`;
+                    break;
+                case 'uploaded_too_close':
+                    errorHtml = `Your fleet has been uploaded recently. Please wait before re-uploading.`;
+                    break;
+                case 'not_found_handle':
+                    errorHtml = `The SC handle ${json.context.handle} does not exist. Please check the typo.`;
+                    break;
+                case 'bad_citizen':
+                    errorHtml = `Your SC handle has probably changed. Please update it in <a target="_blank" href="${fleetManagerBaseUrl}/#/profile">your Profile</a>.`;
+                    break;
+                case 'invalid_fleet_data':
+                case 'bad_json':
+                case 'cannot_handle_file':
+                    errorHtml = `An error has occurred, please retry. If this error persists, you can <a href="https://github.com/Ioni14/fleet-manager-extension/issues">post an issue on the repo</a> to help us to resolve it.`;
+                    break;
+            }
+            $('#FME-exporter-msg').html(errorHtml);
         } else if (resp.status === 403) {
             window.localStorage.removeItem('apiToken');
-            $('#FME-exporter-block').append(`<p id="FME-exporter-msg">We can't upload your fleet. Please login first at <a href="${fleetManagerBaseUrl}" target="_blank">${fleetManagerBaseUrl}</a>.</p>`);
+            $('#FME-exporter-msg').html(`We can't upload your fleet. Please login first at <a href="${fleetManagerBaseUrl}" target="_blank">${fleetManagerBaseUrl}</a>.`);
+        } else {
+            $('#FME-exporter-msg').html(`An error has occurred, please retry. If this error persists, you can <a href="https://github.com/Ioni14/fleet-manager-extension/issues">post an issue on the repo</a> to help us to resolve it.`);
         }
     });
 })();
