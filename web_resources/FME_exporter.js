c0728b68a208a434d677448511f8047f6f017497
Thomas Talbot <thomas.talbot@fauya.fr>
2019-05-12 23:02:18 +0200

    add pagination

--- web_resources/FME_exporter.js
+++ web_resources/FME_exporter.js
@@ -2,6 +2,21 @@ $(function () {
     'use strict';
     const fleetManagerBaseUrl = 'https://fleet.fallkrom.space';
 
+    let body = document.body;
+    if (document.location.search !== '?page=1&pagesize=100') {
+        const url = '/account/pledges?page=1&pagesize=100';
+        const $page = $('<div>');
+        $page.load(url + ' .page-wrapper', function (response, status) {
+            if (status === "success") {
+                process(this);
+            } else {
+                $('.sidenav').append('Error loading your pledges. You can <a href="https://github.com/Ioni14/fleet-manager-extension/issues">post an issue on the repo</a> to help us.');
+            }
+        });
+    } else {
+        process(body);
+    }
+
     function createExporterBlock() {
         const exporterBlockHtml = `
             <div id="FME-exporter-block" style="margin-top:20px;">
@@ -24,6 +39,24 @@ $(function () {
         $('.sidenav').append(exporterBlockHtml);
     }
 
+    async function retrieveApiToken() {
+        const resp = await fetch(fleetManagerBaseUrl + '/api/me', {
+            credentials: 'include'
+        });
+        if (resp.ok) {
+            const json = await resp.json();
+
+            return json.apiToken;
+        }
+        if (resp.status === 401 || resp.status === 403) {
+            $('#FME-exporter-msg').html(`We can't upload your fleet. Please login first at <a href="${fleetManagerBaseUrl}" target="_blank">${fleetManagerBaseUrl}</a>.`);
+        } else {
+            $('#FME-exporter-msg').html(`Unable to request Fleet Manager, please retry. If this error persists, you can <a href="https://github.com/Ioni14/fleet-manager-extension/issues">post an issue on the repo</a> to help us to resolve it.`);
+        }
+
+        return null;
+    }
+
     const _manufacturerShortMap = {
         'ANVL': 'Anvil',
         'AEGS': 'Aegis',
@@ -43,106 +76,96 @@ $(function () {
         'XIAN': 'Xi\'an',
     };
 
-    let pledges = [];
-    $('.list-items li').each((index, el) => {
-        const $pledge = $(el);
+    function process(body) {
+        let pledges = [];
+        $('.list-items li', body).each((index, el) => {
+            const $pledge = $(el);
 
-        const $shipInfo = $pledge.find('.kind:contains(Ship)').parent();
-        if ($shipInfo.length === 0) {
-            return;
-        }
+            $('.items .item', $pledge).each((indexItem, elItem) => {
+                const $item = $(elItem);
 
-        const pledge = {
-            id: $('.js-pledge-id', $pledge).val(),
-            name: $('.title', $shipInfo).text(),
-            cost: $('.js-pledge-value', $pledge).val(),
-            lti: $('.title:contains(Lifetime Insurance)', $pledge).length > 0,
-            manufacturer: $('.liner span', $shipInfo).text(),
-            pledge: $('.js-pledge-name', $pledge).val(),
-            pledge_date: $('.date-col:first', $pledge).text().replace(/created:\s+/gi, '').trim(),
-        };
-        pledge.name = pledge.name.replace(/^\s*(?:Aegis|Anvil|Banu|Drake|Esperia|Kruger|MISC|Origin|RSI|Tumbril|Vanduul|Xi'an)[^a-z0-9]+/gi, '');
-        pledge.name = pledge.name.replace(/^\s*(?:Aegis|Anvil|Banu|Drake|Esperia|Kruger|MISC|Origin|RSI|Tumbril|Vanduul|Xi'an)[^a-z0-9]+/gi, '');
-        pledge.manufacturer = _manufacturerShortMap[pledge.manufacturer] || pledge.manufacturer;
-        pledge.warbond = pledge.name.toLowerCase().indexOf('warbond') > -1;
+                const $shipInfo = $item.find('.kind:contains(Ship)').parent();
+                if ($shipInfo.length === 0) {
+                    return;
+                }
 
-        pledges.push(pledge);
-    });
-    createExporterBlock();
+                const pledge = {
+                    id: $('.js-pledge-id', $pledge).val(),
+                    name: $('.title', $shipInfo).text(),
+                    cost: $('.js-pledge-value', $pledge).val(),
+                    lti: $('.title:contains(Lifetime Insurance)', $pledge).length > 0,
+                    manufacturer: $('.liner span', $shipInfo).text(),
+                    package_id: $('.js-pledge-id', $pledge).val(),
+                    pledge: $('.js-pledge-name', $pledge).val(),
+                    pledge_date: $('.date-col:first', $pledge).text().replace(/created:\s+/gi, '').trim(),
+                };
+                pledge.name = pledge.name.replace(/^\s*(?:Aegis|Anvil|Banu|Drake|Esperia|Kruger|MISC|Origin|RSI|Tumbril|Vanduul|Xi'an)[^a-z0-9]+/gi, '');
+                pledge.name = pledge.name.replace(/^\s*(?:Aegis|Anvil|Banu|Drake|Esperia|Kruger|MISC|Origin|RSI|Tumbril|Vanduul|Xi'an)[^a-z0-9]+/gi, '');
+                pledge.manufacturer = _manufacturerShortMap[pledge.manufacturer] || pledge.manufacturer;
+                pledge.warbond = pledge.name.toLowerCase().indexOf('warbond') > -1;
 
-    async function retrieveApiToken() {
-        const resp = await fetch(fleetManagerBaseUrl + '/api/me', {
-            credentials: 'include'
+                pledges.push(pledge);
+            });
         });
-        if (resp.ok) {
-            const json = await resp.json();
+        createExporterBlock();
+        console.log(pledges);
 
-            return json.apiToken;
-        }
-        if (resp.status === 401 || resp.status === 403) {
-            $('#FME-exporter-msg').html(`We can't upload your fleet. Please login first at <a href="${fleetManagerBaseUrl}" target="_blank">${fleetManagerBaseUrl}</a>.`);
-        } else {
-            $('#FME-exporter-msg').html(`Unable to request Fleet Manager, please retry. If this error persists, you can <a href="https://github.com/Ioni14/fleet-manager-extension/issues">post an issue on the repo</a> to help us to resolve it.`);
-        }
+        $('#FME-exporter-submit').on('click', async (ev) => {
+            $('#FME-exporter-msg').html('');
 
-        return null;
-    }
-
-    $('#FME-exporter-submit').on('click', async (ev) => {
-        $('#FME-exporter-msg').html('');
-
-        let apiToken = window.localStorage.getItem('apiToken');
-        if (!apiToken) {
-            apiToken = await retrieveApiToken();
-            window.localStorage.setItem('apiToken', apiToken);
-        }
-        if (!apiToken) {
-            return;
-        }
-
-        $('#FME-exporter-msg').html(`Uploading...`);
-        const resp = await fetch(fleetManagerBaseUrl + '/api/export', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + apiToken
-            },
-            body: JSON.stringify(pledges)
-        });
-        if (resp.ok) {
-            $('#FME-exporter-msg').html(`<span style="color: #0f0;">Success!</span> <a target="_blank" href="${fleetManagerBaseUrl}/#/my-fleet">Go to your fleet</a>.`);
-            return;
-        }
-        if (resp.status === 400) {
-            const json = await resp.json();
-            console.error(`An error has occurred. Error: ${json.error}.`);
-
-            let errorHtml = '';
-            switch (json.error) {
-                case 'no_citizen_created':
-                    errorHtml = `Your RSI account must be linked first. Go to the <a target="_blank" href="${fleetManagerBaseUrl}/#/profile">profile page</a>.`;
-                    break;
-                case 'uploaded_too_close':
-                    errorHtml = `Your fleet has been uploaded recently. Please wait before re-uploading.`;
-                    break;
-                case 'not_found_handle':
-                    errorHtml = `The SC handle ${json.context.handle} does not exist. Please check the typo.`;
-                    break;
-                case 'bad_citizen':
-                    errorHtml = `Your SC handle has probably changed. Please update it in <a target="_blank" href="${fleetManagerBaseUrl}/#/profile">your Profile</a>.`;
-                    break;
-                case 'invalid_fleet_data':
-                case 'bad_json':
-                case 'cannot_handle_file':
-                    errorHtml = `An error has occurred, please retry. If this error persists, you can <a href="https://github.com/Ioni14/fleet-manager-extension/issues">post an issue on the repo</a> to help us to resolve it.`;
-                    break;
+            let apiToken = window.localStorage.getItem('apiToken');
+            if (!apiToken) {
+                apiToken = await retrieveApiToken();
+                window.localStorage.setItem('apiToken', apiToken);
             }
-            $('#FME-exporter-msg').html(errorHtml);
-        } else if (resp.status === 403) {
-            window.localStorage.removeItem('apiToken');
-            $('#FME-exporter-msg').html(`We can't upload your fleet. Please login first at <a href="${fleetManagerBaseUrl}" target="_blank">${fleetManagerBaseUrl}</a>.`);
-        } else {
-            $('#FME-exporter-msg').html(`An error has occurred, please retry. If this error persists, you can <a href="https://github.com/Ioni14/fleet-manager-extension/issues">post an issue on the repo</a> to help us to resolve it.`);
-        }
-    });
+            if (!apiToken) {
+                return;
+            }
+
+            $('#FME-exporter-msg').html(`Uploading...`);
+            const resp = await fetch(fleetManagerBaseUrl + '/api/export', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + apiToken
+                },
+                body: JSON.stringify(pledges)
+            });
+            if (resp.ok) {
+                $('#FME-exporter-msg').html(`<span style="color: #0f0;">Success!</span> <a target="_blank" href="${fleetManagerBaseUrl}/#/my-fleet">Go to your fleet</a>.`);
+                return;
+            }
+            if (resp.status === 400) {
+                const json = await resp.json();
+                console.error(`An error has occurred. Error: ${json.error}.`);
+
+                let errorHtml = '';
+                switch (json.error) {
+                    case 'no_citizen_created':
+                        errorHtml = `Your RSI account must be linked first. Go to the <a target="_blank" href="${fleetManagerBaseUrl}/#/profile">profile page</a>.`;
+                        break;
+                    case 'uploaded_too_close':
+                        errorHtml = `Your fleet has been uploaded recently. Please wait before re-uploading.`;
+                        break;
+                    case 'not_found_handle':
+                        errorHtml = `The SC handle ${json.context.handle} does not exist. Please check the typo.`;
+                        break;
+                    case 'bad_citizen':
+                        errorHtml = `Your SC handle has probably changed. Please update it in <a target="_blank" href="${fleetManagerBaseUrl}/#/profile">your Profile</a>.`;
+                        break;
+                    case 'invalid_fleet_data':
+                    case 'bad_json':
+                    case 'cannot_handle_file':
+                        errorHtml = `An error has occurred, please retry. If this error persists, you can <a href="https://github.com/Ioni14/fleet-manager-extension/issues">post an issue on the repo</a> to help us to resolve it.`;
+                        break;
+                }
+                $('#FME-exporter-msg').html(errorHtml);
+            } else if (resp.status === 403) {
+                window.localStorage.removeItem('apiToken');
+                $('#FME-exporter-msg').html(`We can't upload your fleet. Please login first at <a href="${fleetManagerBaseUrl}" target="_blank">${fleetManagerBaseUrl}</a>.`);
+            } else {
+                $('#FME-exporter-msg').html(`An error has occurred, please retry. If this error persists, you can <a href="https://github.com/Ioni14/fleet-manager-extension/issues">post an issue on the repo</a> to help us to resolve it.`);
+            }
+        });
+    }
 });
